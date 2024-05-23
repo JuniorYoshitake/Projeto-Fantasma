b5340b9573cfc96bf2daaec8e3495d0b7d05e7c8
JuniorYoshitake <1junioryoshitake@gmail.com>
2024-05-23 20:54:20 -0300

    correcao analise 3

--- rdocs/JuniorYoshitake.R
+++ rdocs/JuniorYoshitake.R
@@ -382,23 +382,114 @@ dados_gerais <- data.frame(Media = media_todas_temporadas, Variance = variancia_
  
  resumo4 <- data.frame(IMDB = banco4$imdb, engajamento = banco4$engagement)
  
- correlacao <- cor(resumo4$IMDB, resumo4$engajamento, use = "complete.obs")
+ correlacao <- cor(resumo4$IMDB, resumo4$engajamento,method = "pearson",  use = "complete.obs")
  
  modelo <- lm(engagemento ~ IMDB, data = resumo4)
  
  mediaimdb <- mean(resumo4$IMDB)
  
  mediaeng <- mean(resumo4$engajamento)
+
+
+ 
  #grafico 4
  
  ggplot(resumo4) +
    aes(x = IMDB, y = engajamento) +
-   geom_point(colour = "#A11D21", size = 3) +
+   geom_point(colour = "#A11D21", size = 3, alpha = 0.5) +
    geom_smooth(method = "lm", color = "#003366", se = FALSE) +
    labs(
      x = "Nota IMDB",
      y = "Engajamento"
-   ) +
+   ) + scale_x_continuous(limits = c(2, NA)) +
    theme_estat()
  ggsave(filename = file.path(caminho_junior, "analise-4-disp-bi-freq.pdf"), width = 158, height = 93, units = "mm")
+ 
+ 
+ #QUADRO 
+ 
+ print_quadro_resumo <- function(data, var_name, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+ {
+   var_name <- substitute(var_name)
+   data <- data %>%
+     summarize(`Média` = round(mean(!!sym(var_name)),2),
+               `Desvio Padrão` = round(sd(!!sym(var_name)),2),
+               `Variância` = round(var(!!sym(var_name)),2),
+               `Mínimo` = round(min(!!sym(var_name)),2),
+               `1º Quartil` = round(quantile(!!sym(var_name), probs = .25),2),
+               `Mediana` = round(quantile(!!sym(var_name), probs = .5),2),
+               `3º Quartil` = round(quantile(!!sym(var_name), probs = .75),2),
+               `Máximo` = round(max(!!sym(var_name)),2)) %>%
+     t() %>% 
+     as.data.frame() %>%
+     rownames_to_column()
+   
+   latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+   
+   col_count <- ncol(data)
+   row_count <- nrow(data)
+   latex <- str_c(latex, "| l |\n", sep=" ")
+   for (i in seq(2, col_count))
+   {
+     numCount <- data[i, -c(1)] %>%
+       as.numeric() %>%
+       {floor(log10(.)) + 1} %>%
+       max()
+     latex <- str_c(latex, "\t\t\tS[table-format = ", numCount ,".2]\n", sep="")
+   }
+   
+   
+   latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep="")
+   if (col_count > 2)
+   {
+     for (i in seq(1,col_count))
+     {
+       if (i == 1)
+         latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+       else
+         latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+       
+       if (i < col_count)
+         latex <- str_c(latex, "&", sep=" ")
+       else
+         latex <- str_c(latex, "\\\\\n", sep=" ")
+     }
+   }
+   else
+   {
+     latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+   }
+   
+   latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+   
+   if (col_count > 2)
+     starting_number <- 2
+   else
+     starting_number <- 1
+   
+   for (i in seq(starting_number, row_count))
+   {
+     latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+   }
+   latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+   
+   writeLines(latex)
+ }
+ 
+ #
+ 
+ resumo4 %>%
+   print_quadro_resumo(var_name = IMDB)
+ resumo4 %>%
+   print_quadro_resumo(var_name = engajamento)
+ 
+ variancia <- var(resumo4$engajamento)
  
